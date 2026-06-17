a6c65bbab28d310281295b8fcf6e109d0e64f2c5
Alexxx <sharaputin.aa@gmail.com>
2026-06-18 00:23:13 +0300

    change logs

--- src/proxy.js
+++ src/proxy.js
@@ -1,13 +1,17 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth({
-  pages: {
-    signIn: "/signin",
+export default withAuth(
+  function middleware(req) {
+    console.log("Middleware running for path:", req.nextUrl.pathname);
   },
-  secret: process.env.NEXTAUTH_SECRET,
-});
-console.log("Middleware executed with secret:", process.env.NEXTAUTH_SECRET);
-console.log("Middleware running for path:", req.nextUrl.pathname);
+  {
+    pages: {
+      signIn: "/signin",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+);
+
 export const config = {
   matcher: ["/", "/events/:path*", "/change-password"],
 };
